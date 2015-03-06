96c73cfd2b65e67e1f2ce3df73e19e902228d342
Jeremiah Zhe Liu <zhel90@gmail.com>
2015-03-06 12:33:57 -0500

    Jeremiah: Code Update

--- Script/Jeremiah/classification.py
+++ Script/Jeremiah/classification.py
@@ -68,18 +68,120 @@
 ## for an example. 
 
 import os
+import sys
 from collections import Counter
 try:
     import xml.etree.cElementTree as ET
 except ImportError:
     import xml.etree.ElementTree as ET
+
+#bread and butter
+import nltk
 import numpy as np
 from scipy import stats
 from scipy import sparse
+
+#toyz
+from sklearn import cross_validation as cv
 from sklearn import ensemble as es
 os.chdir("/Users/Jeremiah/GitHub/CS-181-Practical-2/Script/Jeremiah")
 import util
+import pandas as pd
 
+
+################################################
+#### 1. Auxilliary Funcs #######################
+################################################
+def print_full(x):
+    #print all rows of a panda dataframe
+    pd.set_option('display.max_rows', len(x))
+    print(x)
+    pd.reset_option('display.max_rows')
+
+
+def Accuracy(clf_preds, t_train):
+    clf_missId = ((clf_preds - t_train) != 0)
+    clf_miss = t_train[(clf_preds - t_train) != 0]
+    
+    rate = 1 - np.mean(clf_missId) #error rate
+    return rate, clf_miss
+
+def name_for_feature(command, t_train, X_train, feature_dict):
+    try:
+        col_id = feature_dict[command]
+    except KeyError:
+        print ("command not exist")
+        raise KeyError
+        
+    t_train = t_train.reshape(1, np.max(t_train.shape))
+    ftData = X_train.T[col_id]
+
+    nmIdx = t_train[np.array(ftData>0)]
+    out = [util.malware_classes[i] for i in nmIdx]
+    out = stats.itemfreq(out)
+    
+    return out
+
+
+def pruneFeatures(minFreq, X_train_dense, global_feat_dict):
+    print "pruning training features..."
+    sys.stdout.flush()
+
+    #featureFreq = [sum(feature != 0) for feature in X_train_dense.T]
+    #featureFreq_pd = pd.Series(featureFreq, global_feat_dict.keys())
+    featureFreq = []
+    X_train_denseT = X_train_dense.T
+    i = 0
+    i_max = len(X_train_denseT)
+
+    for feature in X_train_denseT:
+        featureFreq.append(sum(feature != 0))
+        i += 1
+        if i % 5000 == 0: 
+            print "Obtaining featureFreq: " + \
+                    str(round(float(i)*100/i_max, 3)) + "%"
+
+    print "obtaining prunId..."
+    sys.stdout.flush()
+
+    prunId = []
+    i = 0
+    for item in featureFreq: 
+        if item > minFreq: prunId.append(i)
+        i += 1
+        if i % 5000 == 0: 
+            print "Obtaining prunId: " + \
+                    str(round(float(i)*100/i_max, 3)) + "%"
+            sys.stdout.flush()
+        
+    print "obtaining prunId...Done!"
+    sys.stdout.flush()
+
+    print "Pruning Design Matrix... "; sys.stdout.flush()
+    X_train_prune = X_train_dense.T[prunId].T #update X
+    print "Pruning Design Matrix...Done!"
+
+    global_feat_dict_prune = Counter() #update global_feat_dict
+    
+    dictList = global_feat_dict.items()
+    dictList_prune = np.array(dictList)[np.array(prunId)]
+    
+    for i in range(len(dictList_prune)):
+        global_feat_dict_prune[dictList_prune[i][0]] = dictList_prune[i][1]
+        if i % 5000 == 0: 
+            print "Pruning feature dict: " + \
+                str(round(float(i)*100/len(dictList_prune), 3)) + "%"
+            sys.stdout.flush()
+
+    print "done pruning training features!"
+    return X_train_prune, global_feat_dict_prune, featureFreq, prunId
+
+    
+################################################
+
+################################
+#### 2. IO functions
+################################
 
 def extract_feats(ffs, direc="train", global_feat_dict=None):
     """
@@ -103,6 +205,10 @@ def extract_feats(ffs, direc="train", global_feat_dict=None):
     fds = [] # list of feature dicts
     classes = []
     ids = [] 
+    
+    i = 0
+    N = str(len(os.listdir(direc)))
+    
     for datafile in os.listdir(direc):
         if datafile == "DS.Store": continue
         # extract id and true class (if available) from filename
@@ -122,6 +228,11 @@ def extract_feats(ffs, direc="train", global_feat_dict=None):
         # accumulate features
         [rowfd.update(ff(tree)) for ff in ffs]
         fds.append(rowfd)
+        
+        i += 1
+        if i%100 == 0: 
+            print "Progress: " + str(i) + "/" + N
+            sys.stdout.flush()
         
     X,feat_dict = make_design_mat(fds,global_feat_dict)
     return X, feat_dict, np.array(classes), ids
@@ -179,11 +290,89 @@ def make_design_mat(fds, global_feat_dict = None):
     
     return X, feat_dict
     
+################################################
+
+################################
+#### 3. Feature Vectors
+################################
 
 ## Here are two example feature-functions. They each take an xml.etree.ElementTree object, 
 # (i.e., the result of parsing an xml file) and returns a dictionary mapping 
 # feature-names to numeric values.
-## TODO: modify these functions, and/or add new ones.
+## TODO: 
+##    1. DLL Type and Address.
+##    2. enum_values, explorer
+##    3. create_namedpipe
+##    4. create_thread_remote
+
+
+def get_all_keys(tree):
+    key_list = Counter()
+    tag_exclusion_set = ["load_dll"]
+    
+    in_all_section = False
+
+    for el in tree.iter():
+        # ignore everything outside the "all_section" element
+        if el.tag == "all_section" and not in_all_section:
+            in_all_section = True
+        elif el.tag == "all_section" and in_all_section:
+            in_all_section = False
+        elif in_all_section:
+            #if el.tag in tag_set
+            for key in el.keys():
+                if key not in tag_exclusion_set:
+                    key_list_name = \
+                        (el.tag + "_" + key + "-" + el.get(key)).lower()
+                    key_list[key_list_name] += 1
+    return key_list
+
+
+def dll_type(tree, name_only = False):
+    #type and address of loaded DLLs
+    dll_list = {"name": [], "extn": [], "addr": []} 
+
+    for el in tree.iter():
+        # obtain DLL target in element
+        if el.tag == "load_dll":
+            dll_name = el.get('filename')
+            #split filename and file_address
+            try:
+                key_bag = dll_name.split("\\")
+                dll_name = "dll_name-" + key_bag[len(key_bag) - 1]
+                dll_addr = "dll_addr-" + "//".join(key_bag[:(len(key_bag) - 1)])
+            except AttributeError:
+                dll_name = "dll_name-DLL_FILE_NOT_SPECIFIED"
+                dll_addr = "dll_addr-" + "//".join(key_bag)
+            
+            if len(dll_name.split(".")) == 2:
+                key_bag = dll_name.split(".")
+                dll_name = key_bag[0]
+                dll_extn = "dll_extn-" + key_bag[1]
+            else:
+                dll_extn = "dll_extn-"
+            
+            dll_name = dll_name.lower()
+            dll_extn = dll_extn.lower()
+            dll_addr = dll_addr.lower()
+            #TODO: convert to lower case
+            dll_list["name"].append(dll_name)
+            dll_list["extn"].append(dll_extn)
+            dll_list["addr"].append(dll_addr)
+
+    dll_list["name"] = stats.itemfreq(dll_list["name"])
+    dll_list["extn"] = stats.itemfreq(dll_list["extn"])    
+    dll_list["addr"] = stats.itemfreq(dll_list["addr"])    
+    dll_list_join = concatenate([dll_list["name"], \
+                    dll_list["extn"], dll_list["addr"]])
+
+    dll_name_counter = Counter()
+    for item in dll_list_join: 
+        dll_name_counter[item[0]] = int(item[1])
+
+    return dll_name_counter
+
+
 def call_freq(tree, name_only = False):
     """
     arguments:
@@ -214,8 +403,14 @@ def call_freq(tree, name_only = False):
         c = set(callz)
     else: 
         c = Counter()
-        for item in freqList: c[item[0]] = int(item[1])
+        for item in freqList: c["sys_call-" +item[0]] = int(item[1])
 
+    return c
+
+
+def call_eigen(tree, call_dict, name_only = False):
+    #TODO: Implement this!
+    c = []
     return c
 
 
@@ -269,6 +464,12 @@ def system_call_count_feats(tree):
             c['num_system_calls'] += 1
     return c
 
+
+################################
+#### 4. Variables of Interest
+################################
+
+
 def call_type(direc):
     names = set() # list of feature dicts
     for datafile in os.listdir(direc):
@@ -276,10 +477,77 @@ def call_type(direc):
         # parse file as an xml document
         tree = ET.parse(os.path.join(direc,datafile))
         # accumulate features
-        newnames = call_freq(tree)
+        newnames = call_freq(tree, name_only = True)
         names = names.union(newnames)
     out_names = names
     return out_names
+
+def call_freq_emp(direc):
+    names = dict() # list of feature dicts
+    for datafile in os.listdir(direc):
+        if datafile == "DS.Store": continue
+        # parse file as an xml document
+        tree = ET.parse(os.path.join(direc,datafile))
+        # accumulate features
+        newnames = call_freq(tree)
+        for k in newnames.iterkeys():
+            if k in names: names[k] = names[k] + newnames[k]
+            else: names[k] = newnames[k]
+            
+    out_names = names
+    return out_names
+    
+def call_freq_byType(direc):
+    #initiate class dependent dictionary
+    name_byType = dict() # list of feature dicts
+    for TypeName in util.malware_classes:
+        name_byType[TypeName] = dict()
+ 
+    for datafile in os.listdir(direc):
+        if datafile == "DS.Store": continue
+        # extract id and true class (if available) from filename
+        id_str,clazz = datafile.split('.')[:2]
+        # parse file as an xml document
+        tree = ET.parse(os.path.join(direc,datafile))
+        # accumulate features: first create new feature
+        newnames = call_freq(tree)
+        # 
+        curNames = name_byType[clazz]
+        for k in newnames.iterkeys():
+            if k in curNames: 
+                curNames[k] = curNames[k] + newnames[k]
+            else: curNames[k] = newnames[k]
+        name_byType[clazz] = curNames
+        
+    out_names = name_byType
+    return out_names
+    
+def dll_type_2(tree, name_only = False):
+    dll_list = {"name": [], "addr": []} 
+
+    for el in tree.iter():
+        # obtain DLL target in element
+        if el.tag == "load_dll":
+            dll_name = el.get('filename')
+            #split filename and file_address
+            key_bag = dll_name.split("\\")
+            
+            dll_name = key_bag[len(key_bag) - 1]
+            dll_addr = "//".join(key_bag[:(len(key_bag) - 1)])
+            #TODO: convert to lower case
+
+            dll_list["name"].append(dll_name)
+            dll_list["addr"].append(dll_addr)
+
+    dll_list["name"] = stats.itemfreq(dll_list["name"])
+    dll_list["addr"] = stats.itemfreq(dll_list["addr"])    
+    dll_list_join = concatenate([dll_list["name"], dll_list["addr"]])
+
+    dll_name_counter = Counter()
+    for item in dll_list_join: 
+        dll_name_counter[item[0]] = int(item[1])
+
+    return dll_name_counter
 
 
 ## The following function does the feature extraction, learning, and prediction
@@ -287,25 +555,126 @@ def main():
     train_dir = "../../../Data/train"
     test_dir = "../../../Data/test"
     outputfile = "../../Output/Jeremiah.csv"  # feel free to change this or take it as an argument
+ 
+    ################################
+    #### Empirical Summary 
+    ################################
+
+    #Get types & fre of commands
     
+    #raw count
+    lesNames =  call_freq(train_dir)
+    lesNames_freq = pd.Series(lesNames.values(), lesNames.keys())
+    lesNames_freq = lesNames_freq/sum(lesNames_freq)
+    lesNames_freq.sort()
+    lesNames_freq
+
+    #raw count by Type
+    lesNames_byType =  call_freq_byType(train_dir)
+    
+    lesNames_byType_freq = dict()
+    for TypeName in util.malware_classes:
+        lesNames_byType_freq[TypeName] = []
+    
+    for keyName in lesNames_byType.keys():
+        namez = lesNames_byType[keyName]
+        namez_freq = pd.Series(namez.values(), namez.keys())
+        namez_freq = namez_freq/sum(namez_freq)
+        namez_freq.sort(ascending = False)
+        lesNames_byType_freq[keyName] = namez_freq[namez_freq > 0.01]
+        print(keyName + " Finished!")
+        sys.stdout.flush()
+    
+    lesNames_byType_freq #most frequent commands in each class
+    
+       
     # TODO put the names of the feature functions you've defined above in this list
-    ffs = [first_last_system_call_feats, system_call_count_feats, call_freq]
+    ffs = [first_last_system_call_feats, system_call_count_feats, \
+            call_freq, dll_type]#, get_all_keys]
+    
+    ################################
+    #### Feature Extraction and Prunning 
+    ################################
 
     # extract features
     print "extracting training features..."
     X_train,global_feat_dict,t_train,train_ids = extract_feats(ffs, train_dir)
     X_train_dense = X_train.todense()
+    del X_train
     print "done extracting training features"
     print
+    sys.stdout.flush()
+    
+    #prunning
+    X_train_prune, global_feat_dict_prune, prunId = \
+    pruneFeatures(minFreq = 5, X_train_dense = X_train_dense, \
+                    global_feat_dict = global_feat_dict)
 
-    # TODO train here, and learn your classification parameters
-    print "learning..."
+    ################################
+    #### CV-based training 
+    ################################
+    n = X_train_dense.shape[0]
     nForest = 1000
-    clf = es.RandomForestClassifier(n_estimators = nForest)
-    clf_fit = clf.fit(X_train_dense, t_train)
-    #learned_W = np.random.random((len(global_feat_dict),len(util.malware_classes)))
+    n_cv = 5
+    
+    print str(n_cv) + " fold learning initiated..."
+    eRate_cv = []
+    kf_cv = cv.KFold(n, n_folds = n_cv)
+    clf_cv = es.RandomForestClassifier(n_estimators = nForest)
+    i = 0
+    
+    for train_index, test_index in kf_cv:
+        i += 1
+        #create CV dataset and fit
+        F_train, F_test = X_train_prune[train_index], X_train_prune[test_index]
+        y_train, y_test = t_train[train_index], t_train[test_index]
+        clf_fit = clf_cv.fit(F_train, y_train)
+        #prediction
+        clf_pred = clf_fit.predict(F_test)
+        accuracy = Accuracy(clf_pred, y_test)[0]
+        eRate_cv.append(accuracy)
+        print("Fold " + str(i) + " Classification Accuracy = " + str(accuracy))
+        sys.stdout.flush()
     print "done learning"
     print
+    
+    np.mean(eRate_cv)
+
+    ################################
+    #feature importance assessment: 
+    ################################
+    # train here, and learn your classification parameters
+    print "learning..."
+    nForest = 1000
+    clf = es.RandomForestClassifier(n_estimators = nForest, \
+                verbose = 1, n_jobs = -1)
+    clf_fit = clf.fit(X_train_dense, t_train)
+    print "done learning"
+    print
+
+    #TODO: Figure out param Name that Feature Importance corresponds to
+    ftImp = pd.DataFrame(sorted(global_feat_dict.keys()), \
+                            columns = ["Name"])
+    ftImp["FeatureImp"] = clf_fit.feature_importances_
+    ftImp_s = ftImp.sort(columns = "FeatureImp", ascending = False)
+
+    print_full(ftImp_s)
+    ftImp_s.loc[ ftImp_s['FeatureImp']> 0.000, :]
+
+    ####################################
+    # in sample prediction and mis-classification rate
+    ####################################
+    
+    print "making in-sample predictions..."
+    clf_preds = clf_fit.predict(X_train_dense)
+    clf_missId = ((clf_preds - t_train) != 0)
+    clf_miss = t_train[(clf_preds - t_train) != 0]
+    
+    rate = 1 - np.mean(clf_missId) #error rate
+    
+    clf_miss = [util.malware_classes[i] for i in clf_miss]
+    stats.itemfreq(clf_miss)
+    print "done making in-sample predictions"
     
     # get rid of training data and load test data
     del X_train
@@ -314,12 +683,14 @@ def main():
     print "extracting test features..."
     X_test,_,t_ignore,test_ids = extract_feats(ffs, test_dir, global_feat_dict=global_feat_dict)
     X_test_dense = X_test.todense()
+    X_test_prune = X_test_dense.T[prunId].T
+    
     print "done extracting test features"
     print
     
     # TODO make predictions on text data and write them out
     print "making predictions..."
-    clf_preds = clf_fit.predict(X_test_dense)
+    clf_preds = clf_fit.predict(X_test_prune)
     print "done making predictions"
     print
     
--- Script/Jeremiah/cuckoo2mist/cuckoo2mist.py
+++ Script/Jeremiah/cuckoo2mist/cuckoo2mist.py
@@ -34,6 +34,8 @@ import hashlib
 import xml.etree.ElementTree as ET
 import glob
 
+os.chdir("/Users/Jeremiah/GitHub/CS-181-Practical-2/Script/Jeremiah/cuckoo2mist")
+
 from thread_mist import th_seq2mist
 
 max_threads	= 10
@@ -78,7 +80,10 @@ def generate_Mist_Reports(files, e2m, t2m):
 					t.join(2.0)
 					if not t.isAlive():
 						thlist.remove(t)
-			t = th_seq2mist(input_file=seqReportRow["seq_path"], elements2mist=e2m, types2mist=t2m, analysis_id=seqReportRow["analysis_id"])
+			t = th_seq2mist(input_file=seqReportRow["seq_path"],
+			elements2mist=e2m, types2mist=t2m, 
+			analysis_id=seqReportRow["analysis_id"])
+			
 			thlist.append(t)
 			t.start()
 	except KeyboardInterrupt:
@@ -97,33 +102,37 @@ def main(argv=None):
 	if argv is None:
 		argv = sys.argv
 	try:
-		try:
-			opts, args = getopt.getopt(argv[1:], "hcio:v", ["help", "config_dir=", "input="])
-		except getopt.error, msg:
-			raise Usage(msg)
+		#try:
+		#	opts, args = getopt.getopt(argv[1:], "hcio:v", ["help", "config_dir=", "input="])
+		#except getopt.error, msg:
+		#	raise Usage(msg)
 			
-		workdir = sys.path[0]
-		os.chdir(workdir)
+		#workdir = sys.path[0]
+		#os.chdir(workdir)
 		
 		f_configdir = "conf"
 		
 		# option processing
-		for option, value in opts:
-			if option == "-v":
-				verbose = True
-			if option in ("-h", "--help"):
-				raise Usage(help_message)
-			if option in ("-o", "--config"):
-				f_configdir = value
-			if option in ("-i", "--input"):
-				f_input = value
-				
+		#for option, value in opts:
+		#	if option == "-v":
+		#		verbose = True
+		#	if option in ("-h", "--help"):
+		#		raise Usage(help_message)
+		#	if option in ("-o", "--config"):
+		#		f_configdir = value
+		#	if option in ("-i", "--input"):
+		#		f_input = value
+		verbose = True
+		
+						
 		print "Reading configuration files from %s ..." % (f_configdir), 
 		(e2m, t2m) = read_configuration(f_configdir)
 		print " done."
 		
-		log_md5s_before = get_log_md5s()
+		#log_md5s_before = get_log_md5s()
 		
+		f_input = "../../../../Data/train"
+
 		print "Reading %s" % (f_input),
 		files = []
 		if os.path.exists(f_input):
